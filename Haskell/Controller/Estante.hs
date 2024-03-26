31141f275e3ed01ac4a882f69c56533b6554ed39
hennanhfalcao <hennan.heim.falcao@ccc.ufcg.edu.br>
2024-03-25 23:13:47 -0300

    atualizado

--- Haskell/Controller/Estante.hs
+++ Haskell/Controller/Estante.hs
@@ -1,54 +1,64 @@
 {-# LANGUAGE DeriveGeneric #-}
-
+{-# LANGUAGE OverloadedStrings #-}
 module Controller.Estante where
 
-import Data.Aeson 
-import Data.Aeson.Encode.Pretty (encodePretty)
-import Data.Maybe
-import GHC.Generics
-import qualified Data.ByteString.Lazy as BS
 import System.Directory
+import GHC.Generics
+import Data.Aeson
+import Data.Maybe
+import qualified Data.ByteString.Lazy as BL
+import Data.Aeson.Encode.Pretty (encodePretty)
 import System.IO.Unsafe
+import System.IO
+import Data.Bool (Bool)
+import Data.Maybe (fromMaybe)
+import Haskell.Controller.Livro
 
+{-Criando estabtes, que podem ser dos subtipos: Lendo, Lidos, Pretendo Ler e Abandonados-}
 data Estante = Lendo | Lidos | Pretendo_Ler | Abandonados {
     livros :: [Livro],
 } deriving (Show, Generic)
 
-instance ToJSON Usuario 
-instance FromJSON Usuario
+instance ToJSON Estante 
+instance FromJSON Estante
+
 {- Adiciona um livro já existente no sistema  a um tipo de estante-}
-adicionaLivro :: String -> Estante -> Livro
-if procuraLivro nome Livro.livros  != Nothing then
-    case estante of
-        Lendo ->
-            adicionaLivro nome estante = do
-            let novoLivro = encodePretty (entrada : estante)
-            BS.writefile "Data/temp.json" novoLivro
-            removeFile "Data/lendo.json"
-            renameFile "Data/temp.json" "Data/lendo.json"
-        Lidos ->
-            adicionaLivro nome estante = do
-            let novoLivro = encodePretty (entrada : estante)
-            BS.writefile "Data/temp.json" novoLivro
-            removeFile "Data/lidos.json"
-            renameFile "Data/temp.json" "Data/lidos.json"   
-        Pretendo_Ler ->
-            adicionaLivro nome estante = do
-            let novoLivro = encodePretty (entrada : estante)
-            BS.writefile "Data/temp.json" novoLivro
-            removeFile "Data/pretendo_ler.json"
-            renameFile "Data/temp.json" "Data/pretendo_ler.json"
-        Abandonados ->
-            adicionaLivro nome estante = do
-            let novoLivro = encodePretty (entrada : estante)
-            BS.writefile "Data/temp.json" novoLivro
-            removeFile "Data/abandonados.json"
-            renameFile "Data/temp.json" "Data/abandonados.json" 
+adicionaLivro :: String -> Estante -> String -> IO()
+adicionaLivro nome estante = do
+    if verificaLivro nome then
+        case estante of
+            Lendo ->
+                adicionaLivro nome estante = do
+                let novoLivro = encodePretty (entrada : estante)
+                BS.writefile "Data/temp.json" novoLivro
+                removeFile "Data/lendo.json"
+                renameFile "Data/temp.json" "Data/lendo.json"
+                putStrLn "Livro adicionado à estante dos livros que você está lendo atualmente."
+            Lidos ->
+                adicionaLivro nome estante = do
+                let novoLivro = encodePretty (entrada : estante)
+                BS.writefile "Data/temp.json" novoLivro
+                removeFile "Data/lidos.json"
+                renameFile "Data/temp.json" "Data/lidos.json"
+                putStrLn "Livro adicionado à estante dos livros que você está já leu."   
+            Pretendo_Ler ->
+                adicionaLivro nome estante = do
+                let novoLivro = encodePretty (entrada : estante)
+                BS.writefile "Data/temp.json" novoLivro
+                removeFile "Data/pretendo_ler.json"
+                renameFile "Data/temp.json" "Data/pretendo_ler.json"
+                putStrLn "Livro adicionado à estante dos livros que você ainda pretende ler."
+            Abandonados ->
+                adicionaLivro nome estante = do
+                let novoLivro = encodePretty (entrada : estante)
+                BS.writefile "Data/temp.json" novoLivro
+                removeFile "Data/abandonados.json"
+                renameFile "Data/temp.json" "Data/abandonados.json"
+                putStrLn "Livro adicionado à estante dos livros que você abandonou a leitura." 
 
-
-{-Procura um livro no sistema e o recupera-}
-procuraLivro :: String -> [Livro] -> Maybe Livro
-procuraLivro [] _ = Nothing
-procuraLivro nome (x:xs) | nome == (Livro.nome x) = livro
-                                | otherwise = procuraLivro nome xs
+{-Verifica se o livro já está cadastrado no sistema bem como se já foi adicionado a alguma das estantes-}
+verificaLivro :: String -> Bool
+if Livro.livroJaExiste nome listaLivro then    
+    if nome not in Lendo.livros && Lidos.livros && Pretendo_Ler.livros && Abandonados.livros then False else True
+else False
                 
