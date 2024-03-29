ec18a8358ead1668fe2f996b4d23efa8ce7db6ca
Iorran Lira <iorran.santos.lira@ccc.ufcg.edu.br>
2024-03-29 17:39:00 -0300

    livros update

--- Haskell/Controller/Livro.hs
+++ Haskell/Controller/Livro.hs
@@ -16,7 +16,8 @@ import Data.Maybe (fromMaybe)
 instance FromJSON Livro
 instance ToJSON Livro
 
--- Criando um tipo de dado chamado "Livro".
+-- |Criando um tipo de dado representando um Livro.
+-- |Um Livro tem nome, autor, um número de páginas e o gênero referente.
 data Livro = Livro {
     nome :: String,
     autor :: String,
@@ -24,7 +25,10 @@ data Livro = Livro {
     genero :: String
 }   deriving (Show, Generic)
 
--- Cadastra um livro no sistema
+
+-- |Método Interativo que cadastra um Livro no sistema,
+-- |usa o nome para verificar se o Livro já está cadastrado,
+-- |chama o método auxiliar para manipular a atualização do sistema de Livros.
 cadastraLivro :: String -> String -> Int -> String -> IO()
 cadastraLivro nome autor n_paginas genero = do
     if livroJaExiste nome getListaLivros then
@@ -35,8 +39,8 @@ cadastraLivro nome autor n_paginas genero = do
         escreveLivro (fromJust maybeJson) livro
         putStrLn "Livro cadastrado."
 
-
--- Escreve um livro em um arquivo Json
+-- |Método Interativo auxiliar que escreve os dados do Livro no sistema,
+-- |usasse um arquivo .json para armazenamento.
 escreveLivro :: [Livro] -> Livro -> IO()
 escreveLivro listaLivros livro = do
     let novoLivro = encodePretty (livro : listaLivros)
@@ -44,7 +48,9 @@ escreveLivro listaLivros livro = do
     removeFile "Data/livros.json"
     renameFile "Data/temp.json" "Data/livros.json"
 
-
+-- |Método interativo que deleta um Livro do sistema a partir do nome,
+-- |se o Livro existir, chama o método auxiliar para excluí-lo da lista.
+-- |Ao final, atualiza o sistema. 
 deletaLivro :: String -> IO ()
 deletaLivro nomeLivro = do
     if livroJaExiste nomeLivro getListaLivros then do
@@ -58,18 +64,20 @@ deletaLivro nomeLivro = do
     else
         putStrLn "Livro não existe no sistema."
 
+-- |Método recursivo auxiliar que exclui um Livro da lista de Livros do sistema.
 removeLivro :: String -> [Livro] -> [Livro]
 removeLivro _ [] = []
 removeLivro nomeLivro (h:t)
     |nome h == nomeLivro = t
     |otherwise = h : removeLivro nomeLivro t
 
-
+-- |Método Interativo que recupera a lista de todos os Livros cadastrados no sistema.
 getListaLivros :: IO (Maybe [Livro])
 getListaLivros = do
     arquivo <- BL.readFile "Data/livros.json"
     return (decode arquivo)
 
+-- |Método recursivo que recupera um Livro cadastrado na lista de Livros a partir do nome.
 getLivro :: String -> [Livro] -> Maybe Livro
 getLivro _ [] = Nothing
 getLivro nomeProcurado (h:t)
@@ -77,6 +85,7 @@ getLivro nomeProcurado (h:t)
     | otherwise = getLivro nomeProcurado t
 
 
+-- |Método Interativo que verifica se um Livro já existe no sistema.
 livroJaExiste :: String -> IO(Maybe [Livro]) -> Bool
 livroJaExiste nomeProcurado listaLivro =
     case maybeListaLivros of
